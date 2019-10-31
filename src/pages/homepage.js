db23226ac9ef70843be40d0fcceeafb6f1de57e6
Dan Shaw <d.shaw@hotter.com>
2019-10-31 11:24:35 +0000

    Update homepage.js

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -5,13 +5,13 @@ import Hotspots from '../components/hotspot/hotspots';
 
 import AmplienceSlot from '../components/amplienceSlot';
 
-import loadingIndicator from '../components/loadingIndicator';
+import LoadingIndicator from '../components/loadingIndicator';
 
 function HomePage(){
     return (
         <div id="homepage">
-            <AmplienceSlot contentToRender={Banner} slotId='2cb34398-4170-4694-a3f0-5d85de451537' slotType='https://www.banner.slot' loadingIndicator={()=><loadingIndicator height={400}/>}/>
-            <AmplienceSlot contentToRender={Hotspots} slotId='b2f83a21-c630-4f0f-9cc7-53af4800e8e6' slotType='https://www.hotspot.slot' loadingIndicator={()=><loadingIndicator height={800}/>}/>
+            <AmplienceSlot contentToRender={Banner} slotId='2cb34398-4170-4694-a3f0-5d85de451537' slotType='https://www.banner.slot' loadingIndicator={()=><LoadingIndicator height={400}/>}/>
+            <AmplienceSlot contentToRender={Hotspots} slotId='b2f83a21-c630-4f0f-9cc7-53af4800e8e6' slotType='https://www.hotspot.slot' loadingIndicator={()=><LoadingIndicator height={800}/>}/>
         </div>
     )
 }
